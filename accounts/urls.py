6e5b28956702e4a5179d39276b032625d9575f53
thanhnhan <thanhnhan959604@gmail.com>
2026-06-13 23:39:08 +0700

    add 4 file url, admin, auth_url, decorator, test

---
+++ accounts/urls.py
@@ -0,0 +1,37 @@
+#namespace URL dành riêng cho endpoint quản lý tài khoảng
+#/api/v1/accounts/
+
+from django.urls import path
+from accounts.views import (
+    MyProfileView,
+    AvatarUploadView,
+    PrivacyView,
+    PublicProfileView,
+    BlockView,
+    ArtistVerificationView,
+    AdminVerificationListView,
+    AdminVerificationApproveView,
+    AdminVerificationRejectView,
+)
+
+urlpatterns = [
+    #hồ sơ cá nhân
+    path('me/', MyProfileView.as_view(), name='account-me'),
+    path('me/avatar/', AvatarUploadView.as_view(), name='account-avatar'),
+    path('me/privacy/', PrivacyView.as_view(), name='account-privacy'),
+
+    #hồ sơ công khai
+    path('users/<uuid:user_id>/', PublicProfileView.as_view(), name='account-public-profile'),
+
+    #block
+    path('users/<uuid:user_id>/block/', BlockView.as_view(), name='account-block'),
+
+    #xác thực nghệ sĩ
+    path('artist-verification/', ArtistVerificationView.as_view(), name='artist-verification-submit'),
+    path('artist-verification/me/', ArtistVerificationView.as_view(), name='artist-verfication-me'),
+    
+    #admin
+    path('admin/verifications/', AdminVerificationListView.as_view(), name='admin-verification-list'),
+    path('admin/verifications/<uuid:verification_id>/approve/', AdminVerificationApproveView.as_view(), name='admin-verification-approve'),
+    path('admin/verifications/<uuid:verification_id>/reject/', AdminVerificationRejectView.as_view(), name='admin-verification-reject'),
+]
