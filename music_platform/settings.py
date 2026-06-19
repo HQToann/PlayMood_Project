71b47ffd7834a0e29bfd47884d8adb529b3e5a62
thanhnhan <thanhnhan959604@gmail.com>
2026-06-19 21:16:13 +0700

    chỉnh swuar 2 file setting và urls

--- music_platform/settings.py
+++ music_platform/settings.py
@@ -231,13 +231,13 @@ LOGGING = {
 
 # SQLite override cho môi trường không có PostgreSQL
 # Block này tự detect: nếu không có DB_NAME trong .env thì dùng SQLite
-# import os as _os
-# if not _os.environ.get('DB_NAME'):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db_test.sqlite3',
-#         }
-#     }
-#     # Dùng filesystem storage khi Cloudinary chưa cấu hình
-#     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+import os as _os
+if not _os.environ.get('DB_NAME'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db_test.sqlite3',
+        }
+    }
+    # Dùng filesystem storage khi Cloudinary chưa cấu hình
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
