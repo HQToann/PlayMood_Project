dadb7b7bfd7212386eb03825a8a3af0e56172ed3
thanhnhan <thanhnhan959604@gmail.com>
2026-06-26 23:31:34 +0700

    1 file

--- playlists/views.py
+++ playlists/views.py
@@ -44,7 +44,7 @@ from playlists.services import (
     delete_playlist, 
     add_song_to_playlist, 
     remove_song_from_playlist,
-    reorder_playlist_song,
+    reorder_playlist_songs,
 )
 logger = logging.getLogger(__name__)
 
@@ -343,7 +343,7 @@ class PlaylistSongReorderView(View):
             data = parse_json_body(request)
             validated = validate_reorder(data)
             playlist = get_playlist_detail(playlist_id)
-            reorder_playlist_song(playlist, request.user, validated['song_ids'])
+            reorder_playlist_songs(playlist, request.user, validated['song_ids'])
             return JsonResponse(
                 {
                     'success': True,
