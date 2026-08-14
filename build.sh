6f9a6561bf72c4107e08878fc16684aff5152182
thanhnhan <thanhnhan959604@gmail.com>
2026-08-14 22:44:49 +0700

    fix piplock

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Cài đặt các thư viện trong Pipfile
-pip install pipenv
-pipenv install
-
-# Thu thập các file tĩnh (CSS, JS, Images)
+pip install --upgrade pip
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-# Chạy cập nhật cấu trúc Database
 python manage.py migrate
