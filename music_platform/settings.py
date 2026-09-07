10a7fcb2902e718f236f37a94a018ca5720542b8
QUOCTOAN <huynhquoctoan.dt1979@gmail.com>
2026-09-07 09:06:12 +0700

    Build lại trang web mới trên Render

--- music_platform/settings.py
+++ music_platform/settings.py
@@ -88,17 +88,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'music_platform.wsgi.application'
 ASGI_APPLICATION = 'music_platform.asgi.application'
 
-CHANNEL_LAYERS = {
-    "default": {
-        # 🔧 DEV: Dùng InMemory để không cần Redis khi phát triển local
-        # Khi deploy production: chuyển sang RedisChannelLayer
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-        # "BACKEND": "channels_redis.core.RedisChannelLayer",
-        # "CONFIG": {
-        #     "hosts": [("127.0.0.1", 6379)],
-        # },
-    },
-}
+redis_url = config('REDIS_URL', default='')
+
+if redis_url:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [redis_url],
+            },
+        },
+    }
+else:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+        },
+    }
 
 # --- DATABASE ---
 DATABASES = {
